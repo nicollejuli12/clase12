161b71f40fa9454befd6ebd809a63ab42341b1d7
nicollejuli12 <nicollejuli@hotmail.com>
2021-10-20 21:03:59 -0500

    Revert "Cambios"
    This reverts commit 98578448d90a951e894546a8c9f136ef106d032c.

--- js/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-
-function cont(){
-setTimeout(function(){
-    let valor=document.getElementById('cont');
-    let cantidad=valor.value.length;
-    document.getElementById('RES').innerHTML=cantidad+" caracteres";
-});
-}
--- js/ejercicio6.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button').onclick=function(){
-    let palabra=document.getElementById('palabra').value;
-    let re=new RegExp(palabra,'g');//RegExp se utiliza para hacer coincidir texto con un patrón.
-    let texto=document.getElementById('texto').value;
-    let resultado=texto.replace(re,'<span class="encontrado">$&</span>')
-    document.getElementById('resultado').innerHTML=resultado;
-}
